dfe54079881a6a94cfae9c6fc339b03518e6f06d
Abhinash Dutta <69073394+VectorSigmaOmega@users.noreply.github.com>
2025-11-03 11:18:40 +0530

    Document time and space complexity for Combination Sum
    Added time and space complexity analysis for the Combination Sum algorithm.

---
+++ Striver's SDE Sheet/Recursion/Combination Sum.cpp
@@ -0,0 +1,37 @@
+// Time Complexity: 
+//  max no of branches at a level = N
+//  for 2 levels, no of branches = N^2
+// for D levels, no of branches = N^D
+// D = target/c_min     where c_min is smallest element
+//
+// Space Complexity: Depth of call stack = D. Max size of temp at depth D = D
+//                   Space(stack) + Space(temp) = O(D) + O(D)
+
+class Solution {
+public:
+    vector<vector<int>> result;
+    void findCombination(vector<int> candidates, int target, int index, long sum, vector<int> temp)
+    {
+        for(int i=index; i<candidates.size(); i++)
+        {
+            sum+=candidates[i];
+            temp.push_back(candidates[i]);
+            if(sum<target)
+            {
+                findCombination(candidates, target, i, sum, temp);
+            }
+            else if(sum==target)
+            {
+                result.push_back(temp);
+            }
+            sum-=candidates[i];
+            temp.pop_back();
+        }
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end());
+        vector<int> temp;
+        findCombination(candidates, target, 0, 0, temp);
+        return result;
+    }
+};
