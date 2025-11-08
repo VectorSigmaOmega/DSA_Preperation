7dd4927daafc557b303d928e5e59c4ab40ba5ef2
Abhinash Dutta <69073394+VectorSigmaOmega@users.noreply.github.com>
2025-11-08 06:21:56 +0530

    Update Permutation.cpp

--- Striver's SDE Sheet/Recursion/Permutation.cpp
+++ Striver's SDE Sheet/Recursion/Permutation.cpp
@@ -28,3 +28,28 @@ public:
         return result;
     }
 };
+
+
+// Time complexity: O(n!*n) + O(n!*n) = O(n!*n)
+// Space complexity: O(n)
+class Solution {
+public:
+    vector<vector<int>> result;
+    void findPermutations(vector<int>& nums, int index) {
+        if (index == nums.size()) {
+            result.push_back(nums);
+            return;
+        }
+        for (int i = index; i < nums.size(); i++) {
+            swap(nums[index], nums[i]);
+            findPermutations(nums, index + 1);
+            swap(nums[index], nums[i]);
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        findPermutations(nums, 0);
+        return result;
+    }
+};
+
+
