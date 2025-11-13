6527f2fc3ba3a3f0dd8573018ebf1dbf2d4fee21
Abhinash Dutta <69073394+VectorSigmaOmega@users.noreply.github.com>
2025-11-14 01:17:43 +0530

    Refactor topoSort function to use adjacency list

---
+++ Striver's SDE Sheet/Graph/Kann Algorithm.cpp
@@ -0,0 +1,43 @@
+//Kann or TopoSort BFS
+
+class Solution {
+  public:
+    vector<int> topoSort(int V, vector<vector<int>>& adj) {
+        vector<int> degree(V, 0);
+        //finding degeww
+        for(auto i:adj)
+        {
+            for(auto j : i)
+            {
+                degree[j]++;
+            }
+        }
+        //adding 0 degree nodes to queue
+        queue<int> q;
+        for(int i=0; i<V; i++)
+        {
+            if(degree[i]==0)
+            {
+                q.push(i);
+            }
+        }
+        
+        vector<int> result;
+        
+        while(!q.empty())
+        {
+            int val=q.front();
+            q.pop();
+            result.push_back(val);
+            for(const auto& i : adj[val])
+            {
+                degree[i]--;
+                if(degree[i]==0)
+                {
+                    q.push(i);
+                }
+            }
+        }
+        return result;
+    }
+};
